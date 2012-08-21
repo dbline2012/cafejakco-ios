900a6f5c6aea64e881ab539c7fdf7d4822301c89
Kang Byeol <bkang721@gmail.com>
2012-08-21 23:27:46 +0900

    first commit

---
+++ cafejakco/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  cafejakco
+//
+//  Created by Byeol Kang on 12. 8. 21..
+//  Copyright (c) 2012년 doubleline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ cafejakco/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  cafejakco
+//
+//  Created by Byeol Kang on 12. 8. 21..
+//  Copyright (c) 2012년 doubleline. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
